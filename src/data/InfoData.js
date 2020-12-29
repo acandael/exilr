b5e11b6f23812d5e81f664949236e173ef3aa1b5
acandael <info@anthonycandaele.com>
2020-12-29 13:10:18 +0100

    finish react website

---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImageOne from '../images/interior-1.jpg'
+import ImageTwo from '../images/house-5.jpg'
+
+export const InfoData = {
+  heading: 'Explore our beautiful homes',
+  paragraphOne: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ipsum debitis voluptas soluta aliquid enim odio, cumque consectetur similique dolorum!',
+  paragraphTwo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nam necessitatibus ex at!',
+  buttonLabel: 'View Homes',
+  image: ImageOne,
+  reverse: false,
+  delay: 100
+}
+
+export const InfoDataTwo = {
+  heading: 'Modern Designs',
+  paragraphOne: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ipsum debitis voluptas soluta',
+  paragraphTwo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nam necessitatibus ex at!',
+  buttonLabel: 'View Homes',
+  image: ImageTwo,
+  reverse: true,
+  delay: 300
+}
